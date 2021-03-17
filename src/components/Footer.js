c0ee7f0513b474094a1717210fbb61f4e3972d14
Javier Penaloza <veralece@gmail.com>
2021-03-16 23:35:34 -0700

    updated shared aspects and styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import linkedin from '../images/linkedin.svg';
 function Footer(props) {
     return (
         <footer>
-            <a href="https://www.twitter.com"><img src={twitter} alt="twitter icon"/></a>
-            <a href="mailto:javier.mejia.penaloza@gmail.com"><img src={email} alt="email icon"/></a>
-            <a href="https://www.linkedin.com"><img src={linkedin} alt="linkedin icon"/></a>
+            <a href="https://www.twitter.com"><img className="footer-icon" src={twitter} alt="twitter icon" /></a>
+            <a href="mailto:javier.mejia.penaloza@gmail.com"><img className="footer-icon" src={email} alt="email icon" /></a>
+            <a href="https://www.linkedin.com"><img className="footer-icon" src={linkedin} alt="linkedin icon" /></a>
         </footer>)
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 import logoImg from '../logo_wordmark.svg';
 function Navigation(props) {
 
-    const about = <Link to="./about">About</Link>
-    const resume = <Link to="./resume">Resume</Link>
-    const projects = <Link to="./projects">Projects</Link>
-    const logo = <Link to="./about" id="logo" ><img alt="logo link" src={logoImg} /></Link>
+    const about = <Link className="link" to="./about">About</Link>
+    const resume = <Link className="link" to="./resume">Resume</Link>
+    const projects = <Link className="link" to="./projects">Projects</Link>
+    const logo = <Link to="./about" className="logo" ><img alt="logo link" src={logoImg} /></Link>
     return (
         <>
             <nav id="nav-full">
