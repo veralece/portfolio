cbcdc0ec4409f13a16fbf9627d3dfd2bf7c4a17c
Javier Penaloza <veralece@gmail.com>
2021-03-24 12:12:41 -0700

    updated css and added content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,9 @@ function Footer(props) {
             <a href="https://twitter.com/javy_script"><img className="footer-icon" src={twitter} alt="twitter icon" /></a>
             <a href="mailto:javier.mejia.penaloza@gmail.com"><img className="footer-icon" src={email} alt="email icon" /></a>
             <a href="https://www.linkedin.com/in/javier-penaloza-14ab48181/"><img className="footer-icon" src={linkedin} alt="linkedin icon" /></a>
-            <p className="copyright">Copyright &copy;2021</p>
+            <p className="footer-col-3">Coffee and my cat helped create this site.</p>
+            <p className="footer-col-3"><a href="https://github.com/veralece/portfolio">View Source</a></p>
+            <p className="footer-col-3">&copy; Javier Penaloza 2021. </p>
         </footer>)
 }
 
