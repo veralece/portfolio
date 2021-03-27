9644deb892f041d78992a46cb74afd971f5925ec
Javier Penaloza <veralece@gmail.com>
2021-03-26 18:14:00 -0700

    abstracted hardcoded content into json files

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,66 +1,15 @@
 import EducationListItem from '../components/EducationListItem';
 import WorkListItem from '../components/WorkListItem';
+import educationData from '../content/education.json';
+import experienceData from '../content/experience.json';
 function Resume(props) {
-    const education = [
-        {
-            college: "Arizona State University",
-            degree: "MS, Graphic Information Technology",
-            graduated: false,
-            acquisitionDate: "December 2022"
-        },
-        {
-            college: "Arizona State University",
-            degree: "BAS, Internet & Web Development",
-            graduated: false,
-            acquisitionDate: "July 2021"
-        },
-        {
-
-            college: "Arizona Western College",
-            degree: "AAS, Computer Information Systems",
-            graduated: true,
-            acquisitionDate: "May 2019"
-        }];
-
-    const experience = [
-        {
-            date: "Janurary 2021 - Currently",
-            jobTitle: "Programmer Analyst Assistant",
-            organization: "City of Tempe",
-            duties: [
-                "Maintaining several applications varying from small to large",
-                "Debugging applications",
-                "Writing detailed documentation"
-            ]
-        },
-        {
-            date: "October 2019 - Janurary 2021",
-            jobTitle: "Service Desk Technician II",
-            organization: "Arizona Western College",
-            duties: [
-                "Scripting and automation through PowerShell",
-                "Configuration of Internal Web Applications",
-                "Deploying/managing ITSM System"
-            ]
-        },
-        {
-            date: "Janurary 2017 - October 2019",
-            jobTitle: "Service Desk Technician I",
-            organization: "Arizona Western College",
-            duties: [
-                "Utilizing Microsoft 365 applications to enhance business processes of operational staff",
-                "End-user support",
-                "Deploying Windows images"
-            ]
-        }
-    ];
     return (
         <main>
             <article className="grid" name="Resume">
                 <section className="container shadow">
                     <h1>Education</h1>
                     <ul>
-                        {education.map((e, i) => {
+                        {educationData.education.map((e, i) => {
                             return <EducationListItem
                                 key={i}
                                 college={e.college}
@@ -75,8 +24,8 @@ function Resume(props) {
                 <section className="container shadow">
                     <h1>Work History</h1>
                     <ul>
-                        {experience.map((e,i) => {
-                            return <WorkListItem 
+                        {experienceData.experience.map((e, i) => {
+                            return <WorkListItem
                                 key={i}
                                 organization={e.organization}
                                 jobTitle={e.jobTitle}
