bf696620c0ab7454230335a44d24b33fecb8b6c8
Javi Penaloza <veralece@gmail.com>
2022-08-03 08:18:14 -0700

    updated broken link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer(props) {
         <footer>
             <a href="https://twitter.com/javy_script"><img className="footer-icon" src={twitter} alt="twitter icon" /></a>
             <a href="mailto:javier.mejia.penaloza@gmail.com"><img className="footer-icon" src={email} alt="email icon" /></a>
-            <a href="https://www.linkedin.com/in/javier-p-14ab48181/"><img className="footer-icon" src={linkedin} alt="linkedin icon" /></a>
+            <a href="https://www.linkedin.com/in/javier-penaloza-14ab48181/"><img className="footer-icon" src={linkedin} alt="linkedin icon" /></a>
             <p className="footer-col-3">Coffee and my cat helped create this site.</p>
             <p className="footer-col-3"><a href="https://github.com/veralece/portfolio">View Source</a></p>
             <p className="footer-col-3">&copy; Javier Penaloza 2021. </p>
